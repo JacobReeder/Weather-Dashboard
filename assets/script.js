0cfe10953ea92e33e05afa51caba8e0a962b77e2
JacobReeder <jacob.reeder1@gmail.com>
2022-07-10 21:57:23 -0700

    Reformated html; index.js/updated functions; script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 var textL = $("#city")
 var inCity = $("#cityLabel");
+
+var newIcon = $('<img id="dynamic">');
+
+//Span ID's
+var getTemp = $("#getTemp");
+var getWind = $("#getWind");
+var getHumdty = $("#getHumdty");
+var uvSpan = $("#UvIndex");
+
+//API key
 var apiNew ="5ee8272113a5ff9365424925e09a0196";
+
+
+////////////////////////////////////////
 
 function getTemp(){
     event.preventDefault()
@@ -14,6 +27,8 @@ function getTemp(){
         resetVal();
     } 
 }
+
+////////////////////////////////////////
 
 function getDir(place) {
     var sRoute = geoAPI + "?q=" + city + "&limit=1&appid=" + apiNew;
@@ -43,33 +58,84 @@ function getDir(place) {
 
 }
 
-function getWeather() {
-  
+///////////////////////////////////////////
 
+function getWeather() {
+    var conditionGet = currentCondition + "?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&exclude=hourly,minutely,alerts&units=imperial";
+  
+    fetch(conditionGet).then(function(response) {
+     
+        if (response.status === 200) {
+       
+        response.json().then(function(data) {
+          
+            inCity.text(inCity.text() + ' (' + moment.unix(data.current.dt).format("MM/DD/YYYY") + ')');
+          newIcon.attr('src', iconURL + data.current.weather[0].icon + ".png");
+          newIcon.appendTo(inCity);
+
+          set5dayForecast(data);
+          getTemp.text(data.current.temp + '\u00B0 F');
+          getWind.text(data.current.wind_speed+ ' MPH'); 
+          getHumdty.text(data.current.humidity+ '%');
+          var uvIndex = parseFloat(data.current.uvi);
+          uvSpan.text(uvIndex);
+  
+          uvSpan.removeClass();
+          switch (true) {
+            case (uvIndex<3):
+              uvSpan.addClass("rounded bg-success text-light");
+              break;
+            case (uvIndex>=3 && uvIndex<6):
+              uvSpan.addClass("rounded bgYellow text-dark")
+              break;
+            case (uvIndex>=6 && uvIndex<8):
+              uvSpan.addClass("rounded bg-warning text-light");
+              break;
+            case (uvIndex>=8 && uvIndex<11):
+              uvSpan.addClass("rounded bg-danger text-light");
+              break
+            case (uvIndex>=11):
+              uvSpan.addClass("rounded bgDarkMagenta text-light");
+              break;
+          }
+          
+        })
+        saveCity(inCity.text().trim());
+        showRecentSearches();
+      }
+      else alert("Unable to connect");
+    })
+    .catch(function(error) {
+      alert("Unable to connect");
+    })
     
 }
 
+/////////////////?///////////////
+
 function newSearches() {
- 
+        var myDiv = document.getElementById("currentSearch");
+        removePost(myDiv);
+        if (localStorage.getItem('citySearch')) {
+          existing = localStorage.getItem('citySearch').split(';');
+          for (var i=0; i<existing.length; i++) {
+            var searchClick = document.createElement("button");
+            if (existing[i].trim().length>0) {
+              searchClick.innerText = existing[i];
+              searchClick.addEventListener('click', setCityAndSearch);
+              myDiv.appendChild(searchButton);
+            }
+          }
+        }
 }
+
+function removePost(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+  }
 
 function deleteAll() {
   
 }
 
-function setSearch() {
- 
-}
-
-function getCity() {
-
-}
-
-function newVal() {
- 
-}
-
-function setForecast() {
-
- 
-}
