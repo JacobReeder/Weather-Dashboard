301edfd663d9f6956c68bc3230c29aac312654b6
JacobReeder <jacob.reeder1@gmail.com>
2022-07-10 19:07:39 -0700

    Added variables and initial functions

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,44 @@
+var textL = $("#city")
+var inCity = $("#cityLabel");
+var apiNew ="";
+
 function getTemp(){
-     
+    event.preventDefault()
+    if (textL.val()) {
+      var place = textL.val().trim()
+      getDir(place);
+      textL.val('')
+    }
+    else {
+        inCity.text("WRONG!");
+        resetVal();
+    } 
 }
 
-function getDir() {
+function getDir(place) {
+    var sRoute = geoAPI + "?q=" + city + "&limit=1&appid=" + apiNew;
+    fetch(sRoute).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          if (Object.keys(data).length === 0) {
+    
+            inCity.text("WRONG! YOU LOSE!");
+            resetVal();
+          }
+          else {
+            inCity.text(data[0].name + ", " + data[0].state + ", " + data[0].country);
+            lat = data[0].lat;
+            lon = data[0].lon; 
+            getWeather(place);
+          
+          }
+        })
+      }
+      else alert("Unable to connect, please try again later.");
+    })
+    .catch(function(error) {
+      alert("Unable to connect, please try again later.");
+    })
   
 
 }
